48282ac0886d12f87c5b9dd85184e1bbde81217f
camilo8 <jcpenagos716@misena.edu.co>
2019-02-03 20:42:41 +0100

    Set evaluation

--- Class/Evaluation.php
+++ Class/Evaluation.php
@@ -5,6 +5,28 @@ class Evaluation{
     private $Grade_Id;
     private $Cone;
 
+    public $Id;
+    public $Criterion1;
+    public $Criterion2;
+    public $Criterion3;
+    public $Criterion4;
+    public $Criterion5;
+    public $Criterion6;
+    public $Criterion7;
+    public $Criterion8;
+    public $Criterion9;
+    public $Criterion10;
+    public $Color_Id;
+    public $Student_Id;
+    public $Teacher_Id,
+    public $Commentary_Student;
+    public $Commentary_Teacher;
+    public $End_Procces_Student;
+    public $End_Procces_Teacher;
+    public $Date;
+
+
+
     public function  __construct(){
         $this->Cone = new Conexion();
     }    
@@ -18,6 +40,75 @@ class Evaluation{
          ]);
          $result = $query->fetchALL(\PDO::FETCH_ASSOC);
          return $result;
+    }
+    /*set color aswer */
+     public function ColorAswer($Id,$Criterion1,$Criterion2,$Criterion3,$Criterion4,$Criterion5,$Criterion6,$Criterion7,$Criterion8,$Criterion9,$Criterion10,$Student_Id){
+         $sql="INSERT INTO color(Id,Criterion1,Criterion2,Criterion3,Criterion4,Criterion5,Criterion6,Criterion7,Criterion8,Criterion9,Criterion10,Student_Id) VALUE (:Id,:Criterion1,:Criterion2,:Criterion3,:Criterion4,:Criterion5,:Criterion6,:Criterion7,:Criterion8,:Criterion9,:Criterion10,:Student_Id)";
+         $query=$this->Cone->prepare($sql);   
+         $query->execute([
+             'Id'=>$this->Id;
+             'Criterion1'=>$this->Criterion1,
+             'Criterion2'=>$this->Criterion2,
+             'Criterion3'=>$this->Criterion3,
+             'Criterion4'=>$this->Criterion4,
+             'Criterion5'=>$this->Criterion5,
+             'Criterion6'=>$this->Criterion6,
+             'Criterion7'=>$this->Criterion7,
+             'Criterion8'=>$this->Criterion8,
+             'Criterion9'=>$this->Criterion9,
+             'Criterion10'=>$this->Criterion10,
+             'Student_Id'=>$this->Student_Id
+         ]);
+         $result=$query->fetchALL(\PDO::FECHT_ASSOC);
+         return $result;
+     }
+    /*Set Evaluation Aswer*/
+    public function setAswer($Id,$Criterion1,$Criterion2,$Criterion3,$Criterion4,$Criterion5,$Criterion6,$Criterion7,$Criterion8,$Criterion9,$Criterion10,$Color_Id,$Student_Id,$Teacher_Id,$Commentary_Student,$Commentary_Teacher,$End_Procces_Student,$End_Procces_Teacher,$Date){
+        $sql="INSERT INTO evaluation (Id,Criterion1,Criterion2,Criterion3,Criterion4,Criterion5,Criterion6,Criterion7,Criterion8,Criterion9,Criterion10,Color_Id,Student_Id,Teacher_Id,Commentary_Student,Commentary_Teacher,End_Procces_Student,End_Procces_Teacher,Date)
+        VALUE(:Id,:Criterion1,:Criterion2,:Criterion3,:Criterion4,:Criterion5,:Criterion6,:Criterion7,:Criterion8,:Criterion9,:Criterion10,:Color_Id,:Student_Id,:Teacher_Id,:Commentary_Student,:Commentary_Teacher,:End_Procces_Student,:End_Procces_Teacher,:Date)";
+        $this->Id=$Id;
+        $this->Criterion1=$Criterion1;
+        $this->Criterion2=$Criterion2;
+        $this->Criterion3=$Criterion3;
+        $this->Criterion4=$Criterion4;
+        $this->Criterion5=$Criterion5;
+        $this->Criterion6=$Criterion6;
+        $this->Criterion7=$Criterion7;
+        $this->Criterion8=$Criterion8;
+        $this->Criterion9=$Criterion9;
+        $this->Criterion10=$Criterion10;
+        $this->Color_Id=$Color_Id;
+        $this->Student_Id=$Student_Id;
+        $this->Teacher_Id=$Teacher_Id;
+        $this->Commentary_Student=$Commentary_Student;
+        $this->Commentary_Teacher=$Commentary_Teacher;
+        $this->End_Procces_Student=$End_Procces_Student;
+        $this->End_Procces_Teacher=$End_Procces_Teacher;
+        $this->Date=$Date;
+        $query=$this->Cone->prepare($sql);   
+        $query->execute([
+            'Id'=>$this->Id;
+            'Criterion1'=>$this->Criterion1,
+            'Criterion2'=>$this->Criterion2,
+            'Criterion3'=>$this->Criterion3,
+            'Criterion4'=>$this->Criterion4,
+            'Criterion5'=>$this->Criterion5,
+            'Criterion6'=>$this->Criterion6,
+            'Criterion7'=>$this->Criterion7,
+            'Criterion8'=>$this->Criterion8,
+            'Criterion9'=>$this->Criterion9,
+            'Criterion10'=>$this->Criterion10,
+            'Color_Id'=>$this->Color_Id,
+            'Student_Id'=>$this->Student_Id,
+            'Teacher_Id'=>$this->Teacher_Id,
+            'Commentary_Student'=>$this->Commentary_Student,
+            'Commentary_Teacher'=>$this->Commentary_Teacher,
+            'End_Procces_Student'=>$this->End_Procces_Student,
+            'End_Procces_Teacher'=>$this->End_Procces_Teacher,
+            'Date'=>$this->Date            
+        ]);
+        $result=$query->fetchALL(\PDO::FECHT_ASSOC);
+        return $result;
     }
   
 }
--- JS/ValidColor.js
+++ JS/ValidColor.js
@@ -3,11 +3,11 @@
          var opt = $('#option1').val();
         if(opt === "Seleccionar"){
             $("#color1").val("#ffffff");
-            $("#color11").val("#ffffff");
+            $("#color11").val("ffffff");
         }
         if(opt === "Siempre"){
             $("#color1").val("#009933");
-            $("#color11").val("#009933");
+            $("#color11").val("009933");
         }
         if(opt === "Casi siempre"){
             $("#color1").val("#ffff00");
@@ -15,7 +15,7 @@
         }
         if(opt === "Nunca"){
             $("#color1").val("#ff0000");
-            $("#color11").val("#ff0000");
+            $("#color11").val("ff0000");
 
         }
     });
@@ -24,19 +24,19 @@
         var opt = $('#option2').val();
        if(opt === "Seleccionar"){
            $("#color2").val("#ffffff");
-           $("#color12").val("#ffffff");
+           $("#color12").val("ffffff");
        }
        if(opt === "Siempre"){
            $("#color2").val("#009933");
-           $("#color12").val("#009933");
+           $("#color12").val("009933");
        }
        if(opt === "Casi siempre"){
            $("#color2").val("#ffff00");
-           $("#color12").val("#ffff00");
+           $("#color12").val("ffff00");
        }
        if(opt === "Nunca"){
            $("#color2").val("#ff0000");
-           $("#color12").val("#ff0000");
+           $("#color12").val("ff0000");
        }
    });
 
@@ -44,19 +44,19 @@
     var opt = $('#option3').val();
    if(opt === "Seleccionar"){
        $("#color3").val("#ffffff");
-       $("#color13").val("#ffffff");
+       $("#color13").val("ffffff");
    }
    if(opt === "Siempre"){
        $("#color3").val("#009933");
-       $("#color13").val("#009933");
+       $("#color13").val("009933");
    }
    if(opt === "Casi siempre"){
        $("#color3").val("#ffff00");
-       $("#color13").val("#ffff00");
+       $("#color13").val("ffff00");
    }
    if(opt === "Nunca"){
        $("#color3").val("#ff0000");
-       $("#color13").val("#ff0000");
+       $("#color13").val("ff0000");
    }
 });
 
@@ -64,20 +64,20 @@ $('#option4').change(function(){
     var opt = $('#option4').val();
    if(opt === "Seleccionar"){
        $("#color4").val("#ffffff");
-       $("#color14").val("#ffffff");
+       $("#color14").val("ffffff");
    }
    if(opt === "Siempre"){
         $("#color4").val("#009933");
-        $("#color14").val("#009933");
+        $("#color14").val("009933");
 
     }
    if(opt === "Casi siempre"){
        $("#color4").val("#ffff00");
-       $("#color14").val("#ffff00");
+       $("#color14").val("ffff00");
    }
    if(opt === "Nunca"){
        $("#color4").val("#ff0000");
-       $("#color14").val("#ff0000");
+       $("#color14").val("ff0000");
    }
 });
 
@@ -85,7 +85,7 @@ $('#option5').change(function(){
     var opt = $('#option5').val();
    if(opt === "Seleccionar"){
        $("#color5").val("#ffffff");
-       $("#color15").val("#ffffff");
+       $("#color15").val("ffffff");
    }
    if(opt === "Siempre"){
        $("#color5").val("#009933");
@@ -93,11 +93,11 @@ $('#option5').change(function(){
    }
    if(opt === "Casi siempre"){
        $("#color5").val("#ffff00");
-       $("#color15").val("#ffff00");
+       $("#color15").val("ffff00");
    }
    if(opt === "Nunca"){
        $("#color5").val("#ff0000");
-       $("#color15").val("#ff0000");
+       $("#color15").val("ff0000");
    }
 });
 
@@ -105,19 +105,19 @@ $('#option6').change(function(){
     var opt = $('#option6').val();
    if(opt === "Seleccionar"){
        $("#color6").val("#ffffff");
-       $("#color16").val("#ffffff");
+       $("#color16").val("ffffff");
    }
    if(opt === "Siempre"){
        $("#color6").val("#009933");
-       $("#color16").val("#009933");
+       $("#color16").val("009933");
    }
    if(opt === "Casi siempre"){
        $("#color6").val("#ffff00");
-       $("#color16").val("#ffff00");
+       $("#color16").val("ffff00");
    }
    if(opt === "Nunca"){
        $("#color6").val("#ff0000");
-       $("#color16").val("#ff0000");
+       $("#color16").val("ff0000");
    }
 });
 
@@ -125,19 +125,19 @@ $('#option7').change(function(){
     var opt = $('#option7').val();
    if(opt === "Seleccionar"){
        $("#color7").val("#ffffff");
-       $("#color17").val("#ffffff");
+       $("#color17").val("ffffff");
    }
    if(opt === "Siempre"){
        $("#color7").val("#009933");
-       $("#color17").val("#009933");
+       $("#color17").val("009933");
    }
    if(opt === "Casi siempre"){
        $("#color7").val("#ffff00");
-       $("#color17").val("#ffff00");
+       $("#color17").val("ffff00");
    }
    if(opt === "Nunca"){
        $("#color7").val("#ff0000");
-       $("#color17").val("#ff0000");
+       $("#color17").val("ff0000");
    }
 });
 
@@ -145,19 +145,19 @@ $('#option8').change(function(){
     var opt = $('#option8').val();
    if(opt === "Seleccionar"){
        $("#color8").val("#ffffff");
-       $("#color18").val("#ffffff");
+       $("#color18").val("ffffff");
    }
    if(opt === "Siempre"){
        $("#color8").val("#009933");
-       $("#color18").val("#009933");
+       $("#color18").val("009933");
    }
    if(opt === "Casi siempre"){
        $("#color8").val("#ffff00");
-       $("#color18").val("#ffff00");
+       $("#color18").val("ffff00");
    }
    if(opt === "Nunca"){
        $("#color8").val("#ff0000");
-       $("#color18").val("#ff0000");
+       $("#color18").val("ff0000");
    }
 });
 
@@ -165,19 +165,19 @@ $('#option9').change(function(){
     var opt = $('#option9').val();
    if(opt === "Seleccionar"){
        $("#color9").val("#ffffff");
-       $("#color19").val("#ffffff");
+       $("#color19").val("ffffff");
    }
    if(opt === "Siempre"){
        $("#color9").val("#009933");
-       $("#color19").val("#009933");
+       $("#color19").val("009933");
    }
    if(opt === "Casi siempre"){
        $("#color9").val("#ffff00");
-       $("#color19").val("#ffff00");
+       $("#color19").val("ffff00");
    }
    if(opt === "Nunca"){
        $("#color9").val("#ff0000");
-       $("#color19").val("#ff0000");
+       $("#color19").val("ff0000");
    }
 });
 
@@ -185,19 +185,19 @@ $('#option10').change(function(){
     var opt = $('#option10').val();
    if(opt === "Seleccionar"){
        $("#color10").val("#ffffff");
-       $("#color20").val("#ffffff");
+       $("#color20").val("ffffff");
    }
    if(opt === "Siempre"){
        $("#color10").val("#009933");
-       $("#color20").val("#009933");
+       $("#color20").val("009933");
    }
    if(opt === "Casi siempre"){
        $("#color10").val("#ffff00");
-       $("#color20").val("#ffff00");
+       $("#color20").val("ffff00");
    }
    if(opt === "Nunca"){
        $("#color10").val("#ff0000");
-       $("#color20").val("#ff0000");
+       $("#color20").val("ff0000");
    }
 });
 /*valid of form */
